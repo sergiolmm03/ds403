6c09475f5a9af44db694ce0b0142c1e2ca7f4ed2
Sergio Marques <sergio@unicamp.br>
2022-08-20 00:13:22 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+var express = require('express'),
+    app     = express(),
+    server  = require('http').Server(app),
+    io      = require('socket.io')(server),
+    port    =  process.env.PORT || 8888;
+
+const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'www')));
+
+
+app.get('/', function (req,res) {
+
+    //res.sendFile('/index.html');
+
+    fs.readFile( '/index.html','utf-8', function (err,data) {
+        if (err){
+            res.writeHead(500);
+            return res.end('Erro ao carregar a pagina');
+        }
+        res.writeHead(200, {'Content-Type':"text/html; charset=utf-8"});
+        var result = data;
+        res.end(result);
+    });
+  
+    
+});
+
+
+server.listen(port, ()=> console.log('Escutando na porta '+ port));
