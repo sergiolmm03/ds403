faa2995a984e4a149464cd28d6604c9bd6cebfb9
Sergio Marques <sergio@unicamp.br>
2022-08-20 08:58:11 -0300

    2 correção package json start

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'www')));
 
 
 app.get('/', function (req,res) {
-
+    console.log('Dentro do get');
     res.sendFile('/index.html');
 /*
     fs.readFile( '/index.html','utf-8', function (err,data) {
